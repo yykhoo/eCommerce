38ea16b1b60c1b49c1158b9f37498cbb7463289b
yin yin <yinyin.khoo21@gmail.com>
2019-07-22 18:46:19 +0800

    Initial commit eCommerce template project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainComponent } from './main/main.component';
+import { AccountComponent } from './account/account.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent,
+    AccountComponent
   ],
   imports: [
     BrowserModule,
